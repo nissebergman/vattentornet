00debf83721ae19549ae78b70c4274554e42b962
nibe91 <nibe91@gmail.com>
2020-09-03 16:25:36 +0200

    Updates

---
+++ precache-manifest.7d4c641509ee11d43ea7855db36be552.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00211f61efd9c4233fa6e97761206d05",
+    "url": "/vattentornet/index.html"
+  },
+  {
+    "revision": "4090c06dc6f245954274",
+    "url": "/vattentornet/static/css/main.6817d638.chunk.css"
+  },
+  {
+    "revision": "7bdfa98e6a0726c55d3a",
+    "url": "/vattentornet/static/js/2.7142948c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vattentornet/static/js/2.7142948c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4090c06dc6f245954274",
+    "url": "/vattentornet/static/js/main.5a4d7f07.chunk.js"
+  },
+  {
+    "revision": "d43bc295e4c740a60a4d",
+    "url": "/vattentornet/static/js/runtime-main.21139fd2.js"
+  },
+  {
+    "revision": "56f07c1c19fe849ff8351e9bd5b15672",
+    "url": "/vattentornet/static/media/tornet.56f07c1c.png"
+  }
+]);
