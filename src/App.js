6559581399dd7403079ca8f4a1acde1df0694406
nibe91 <nibe91@gmail.com>
2020-09-03 16:13:06 +0200

    Bytte allt create-react-app fluff mot ett roterande torn. bam

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './tornet.png';
 import './App.css';
 
 function App() {
@@ -7,16 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Extra.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
